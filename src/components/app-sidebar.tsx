245a381cf01c34589f85efa92f2085d887b9613d
Subodh-Bny <adhikarisubodh77@gmail.com>
2024-11-14 19:02:54 +0545

    dashboard page

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,61 @@
+import { FileText, LayoutDashboard, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Blogs",
+    url: "/dashboard/blogs",
+    icon: FileText,
+  },
+  {
+    title: "Settings",
+    url: "/dashboard/settings",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="z-[51]">
+      <SidebarContent className="">
+        <SidebarGroup>
+          <SidebarGroupContent className="py-6 space">
+            <SidebarGroupLabel className="mb-5">
+              <Link href="/" className="text-3xl font-bold text-primary">
+                NextBlog
+              </Link>
+            </SidebarGroupLabel>
+            <SidebarMenu className="pl-4">
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link className="text-lg" href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
